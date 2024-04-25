e139ae3041661ba5ffde4192e787f01cad68ed21
jhethia <jhethia@gmail.com>
2024-04-25 11:06:21 +0530

    corrections in user repository

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Repository/UserRepository.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Integer, UserEntity> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 }
