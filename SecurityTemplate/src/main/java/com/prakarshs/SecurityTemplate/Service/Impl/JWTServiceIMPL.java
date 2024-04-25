a9f4e5971b874c54bd536406a92690167a510e10
jhethia <jhethia@gmail.com>
2024-04-25 11:51:06 +0530

    implemented generate token

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/Impl/JWTServiceIMPL.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/Impl/JWTServiceIMPL.java
@@ -1,9 +1,34 @@
 package com.prakarshs.SecurityTemplate.Service.Impl;
 
+import com.prakarshs.SecurityTemplate.Service.JWTService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
 @Service
-public class JWTServiceIMPL {
+public class JWTServiceIMPL implements JWTService {
 
 
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 *24))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    private Key getSignKey() {
+        byte[] key = Decoders.BASE64.decode(Base64.getEncoder().encodeToString("**@@your-private-key-here@@**".getBytes()));
+        return Keys.hmacShaKeyFor(key);
+    }
 }
--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/JWTService.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/JWTService.java
@@ -1,4 +1,8 @@
 package com.prakarshs.SecurityTemplate.Service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface JWTService {
+    String generateToken(UserDetails userDetails);
+
 }
