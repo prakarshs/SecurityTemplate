01c5f4447e91f5b9543cf99b46e702fb7a132673
jhethia <jhethia@gmail.com>
2024-04-25 10:50:29 +0530

    created user entity

---
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Entity/UserEntity.java
@@ -0,0 +1,29 @@
+package com.prakarshs.SecurityTemplate.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "USER_ENTITY")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    @Column(name = "Fname")
+    private String firstName;
+    @Column(name = "Sname")
+    private String secondName;
+    @Column(name = "Email")
+    private String email;
+    @Column(name = "Password")
+    private String password;
+    @Column(name = "Role")
+    private Role role;
+}
