aa46a1ad1b3d740bf498bc9eefeb65a23df177cd
jhethia <jhethia@gmail.com>
2024-05-03 23:59:32 +0530

    added test hello endpoint UserController

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/UserController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hey There !");
+    }
 
 }
