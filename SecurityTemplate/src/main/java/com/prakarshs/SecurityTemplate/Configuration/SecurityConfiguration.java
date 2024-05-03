5d2f96598b3333eb7fff5ea9efb5faba3518b098
jhethia <jhethia@gmail.com>
2024-05-03 21:14:41 +0530

    added AuthenticationManager

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Configuration/SecurityConfiguration.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Configuration/SecurityConfiguration.java
@@ -7,8 +7,10 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -53,6 +55,11 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
