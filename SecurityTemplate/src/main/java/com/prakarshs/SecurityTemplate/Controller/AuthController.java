e93b39d3236c02d9f1ac6bf71af04146dab12ced
jhethia <jhethia@gmail.com>
2024-05-03 21:18:49 +0530

    made Auth Controller a RestController

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
@@ -1,4 +1,8 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
 }
