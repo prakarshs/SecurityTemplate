a03ce545bcd980c1594ba48e4e9b15b3a416e89a
jhethia <jhethia@gmail.com>
2024-05-04 01:10:28 +0530

    implemented duplicateHandler method

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Exceptions/ErrorHandler.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Exceptions/ErrorHandler.java
@@ -1,5 +1,6 @@
 package com.prakarshs.SecurityTemplate.Exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,7 +8,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<ErrorBody> customErrorHandler(Exception exception){
-
+    public ResponseEntity<ErrorBody> duplicateHandler(DuplicateValueException exception){
+        return new ResponseEntity<>(ErrorBody.builder()
+                .message(exception.getMessage())
+                .resolution(exception.getResolution())
+                .build(), HttpStatus.CONFLICT);
     }
 }
