0fee5c492888151376a4cf4894d09e1bb80ac3ed
jhethia <jhethia@gmail.com>
2024-05-03 23:59:06 +0530

    added test hello endpoint in AdminController

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AdminController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hey There !");
+    }
 }
