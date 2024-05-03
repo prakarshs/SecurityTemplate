a719619eb2fe5affdae5d704aeb515069ab7d326
jhethia <jhethia@gmail.com>
2024-05-03 21:21:02 +0530

    added argsConstructors for authController

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
@@ -1,10 +1,13 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
 }
