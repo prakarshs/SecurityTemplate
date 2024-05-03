1db3f53f083a7525511a67b766af4fe50bb7d7dd
jhethia <jhethia@gmail.com>
2024-05-03 21:33:10 +0530

    autowiring repository and encoder in AuthServiceIMPL

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/Impl/AuthServiceIMPL.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/Impl/AuthServiceIMPL.java
@@ -1,6 +1,17 @@
 package com.prakarshs.SecurityTemplate.Service.Impl;
 
+import com.prakarshs.SecurityTemplate.Repository.UserRepository;
 import com.prakarshs.SecurityTemplate.Service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceIMPL implements AuthService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 }
