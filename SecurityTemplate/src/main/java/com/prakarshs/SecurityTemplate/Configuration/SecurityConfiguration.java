541f6952e0cd3d7b805cb708603cbdfe7ec89d8a
jhethia <jhethia@gmail.com>
2024-05-03 21:07:50 +0530

    created PasswordEncoder method for auhProvider

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Configuration/SecurityConfiguration.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Configuration/SecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -46,6 +47,11 @@ public class SecurityConfiguration {
         authProvider.setUserDetailsService(userService.userDetailService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
     }
 
 }
