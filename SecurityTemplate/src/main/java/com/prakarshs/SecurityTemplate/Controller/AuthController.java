4e58cbc4dc0c0301631d7036d3caf39bf0c750bc
jhethia <jhethia@gmail.com>
2024-05-03 21:20:06 +0530

    added request mapping for authController

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 }
