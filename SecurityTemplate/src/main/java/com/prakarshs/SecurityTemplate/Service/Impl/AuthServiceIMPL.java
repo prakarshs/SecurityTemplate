f88e8e2ec535b147c297c1f282eb0c8a468200c3
jhethia <jhethia@gmail.com>
2024-05-03 22:02:17 +0530

    implemented signup methods

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/Impl/AuthServiceIMPL.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/Impl/AuthServiceIMPL.java
@@ -2,6 +2,7 @@ package com.prakarshs.SecurityTemplate.Service.Impl;
 
 import com.prakarshs.SecurityTemplate.DTO.SignUpRequest;
 import com.prakarshs.SecurityTemplate.Entity.UserEntity;
+import com.prakarshs.SecurityTemplate.Enums.Role;
 import com.prakarshs.SecurityTemplate.Repository.UserRepository;
 import com.prakarshs.SecurityTemplate.Service.AuthService;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,11 @@ public class AuthServiceIMPL implements AuthService {
         UserEntity user = UserEntity.builder()
                 .firstName(signUpRequest.getFirstName())
                 .lastName(signUpRequest.getLastName())
+                .email(signUpRequest.getEmail())
+                .role(Role.USER)
+                .password(passwordEncoder.encode(signUpRequest.getPassword()))
                 .build();
+
+        userRepository.save(user);
     }
 }
