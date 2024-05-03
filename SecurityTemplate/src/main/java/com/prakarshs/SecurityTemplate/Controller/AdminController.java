82a9c708424fbb486f840ef4f397b611e5de34f9
jhethia <jhethia@gmail.com>
2024-05-03 21:22:33 +0530

    added controller anotations in adminController

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AdminController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AdminController.java
@@ -1,4 +1,11 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
 public class AdminController {
 }
