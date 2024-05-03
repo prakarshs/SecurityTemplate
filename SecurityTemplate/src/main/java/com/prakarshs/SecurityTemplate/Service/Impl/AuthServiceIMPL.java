51e3283ad49c4da247b44ca246b940005912024e
jhethia <jhethia@gmail.com>
2024-05-03 22:55:22 +0530

    created signIn methods

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/Impl/AuthServiceIMPL.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Service/Impl/AuthServiceIMPL.java
@@ -7,10 +7,15 @@ import com.prakarshs.SecurityTemplate.Entity.UserEntity;
 import com.prakarshs.SecurityTemplate.Enums.Role;
 import com.prakarshs.SecurityTemplate.Repository.UserRepository;
 import com.prakarshs.SecurityTemplate.Service.AuthService;
+import com.prakarshs.SecurityTemplate.Service.JWTService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +24,10 @@ public class AuthServiceIMPL implements AuthService {
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
-
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JWTService jwtService;
     @Override
     public UserEntity signup(SignUpRequest signUpRequest) {
         UserEntity user = UserEntity.builder()
@@ -37,8 +45,12 @@ public class AuthServiceIMPL implements AuthService {
     @Override
     public JWTAuthResponse signIn(SignInRequest signInRequest) {
 
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signInRequest.getEmail(),signInRequest.getPassword()));
 
-        return null;
+        var user = userRepository.findByEmail(signInRequest.getEmail()).orElseThrow(()-> new IllegalArgumentException("Invalid Email Or Password."));
+
+        var jwt = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(new HashMap<>(), user);
     }
 
 }
