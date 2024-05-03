22ecba5988f4a39f32477528a32625d077eb05a4
jhethia <jhethia@gmail.com>
2024-05-03 22:06:49 +0530

    injected authservice in AuthController

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
+import com.prakarshs.SecurityTemplate.Service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
 
 }
