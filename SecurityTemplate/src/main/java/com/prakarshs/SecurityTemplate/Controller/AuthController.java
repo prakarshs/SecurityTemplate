f223ddbeea2039fda44968bcf7e7978f1a1cf770
jhethia <jhethia@gmail.com>
2024-05-03 22:08:24 +0530

    created signup endpoint

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
@@ -1,9 +1,14 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
+import com.prakarshs.SecurityTemplate.DTO.SignUpRequest;
+import com.prakarshs.SecurityTemplate.Entity.UserEntity;
 import com.prakarshs.SecurityTemplate.Service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +19,11 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserEntity> signup(@RequestBody SignUpRequest signUpRequest){
+
+    }
 
 
 }
