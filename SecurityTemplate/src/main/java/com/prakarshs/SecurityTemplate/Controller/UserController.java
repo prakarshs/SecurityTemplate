490dcc6e74ddf8fc68f39fc097306f94d236848f
jhethia <jhethia@gmail.com>
2024-05-03 21:21:56 +0530

    added controller anotations in userController

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/UserController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/UserController.java
@@ -1,5 +1,12 @@
 package com.prakarshs.SecurityTemplate.Controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
 
 }
