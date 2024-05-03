c01b21ef7f515a02ff724227a967bf2dc7711773
jhethia <jhethia@gmail.com>
2024-05-03 21:06:17 +0530

    implemented DaoAuthenticationProvider and set user values/password encoder

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Configuration/SecurityConfiguration.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Configuration/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -41,6 +42,10 @@ public class SecurityConfiguration {
     }
     @Bean
     public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService.userDetailService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
 }
