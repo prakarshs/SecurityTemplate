21040432cde53bb15de4b05fdf4791adb506c2d0
jhethia <jhethia@gmail.com>
2024-05-03 21:27:03 +0530

    added fields in SignUpRequest

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/DTO/SignUpRequest.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/DTO/SignUpRequest.java
@@ -1,4 +1,12 @@
 package com.prakarshs.SecurityTemplate.DTO;
 
+import lombok.Data;
+
+@Data
 public class SignUpRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
 }
