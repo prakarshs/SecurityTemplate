473465edf3b7d873ffaec3b378b0fd25122e5628
jhethia <jhethia@gmail.com>
2024-05-03 22:12:53 +0530

    added commandlinerunner in springapplication

--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/Controller/AuthController.java
@@ -6,7 +6,6 @@ import com.prakarshs.SecurityTemplate.Service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/SecurityTemplateApplication.java
+++ SecurityTemplate/src/main/java/com/prakarshs/SecurityTemplate/SecurityTemplateApplication.java
@@ -1,16 +1,26 @@
 package com.prakarshs.SecurityTemplate;
 
+import com.prakarshs.SecurityTemplate.Repository.UserRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Log4j2
-public class SecurityTemplateApplication {
+public class SecurityTemplateApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityTemplateApplication.class, args);
 		log.info("*** SECURITY TEMPLATE  STARTED ***");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
